e78d847ff21eeba4d6e52d6b5b4ad4c332b71d25
Samin Islam <436665504@student.sbhs.nsw.edu.au>
2021-07-27 09:39:11 +1000

    settings push

--- Space Invaders/Form1.Designer.vb
+++ Space Invaders/Form1.Designer.vb
@@ -31,19 +31,19 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(297, 182)
+        Me.Button1.Location = New System.Drawing.Point(424, 303)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(498, 218)
+        Me.Button1.Size = New System.Drawing.Size(711, 363)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(420, 333)
-        Me.Button2.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button2.Location = New System.Drawing.Point(600, 555)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(226, 187)
+        Me.Button2.Size = New System.Drawing.Size(323, 312)
         Me.Button2.TabIndex = 1
         Me.Button2.Text = "Button2"
         Me.Button2.UseVisualStyleBackColor = True
@@ -52,44 +52,49 @@ Partial Class Form1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label1.Location = New System.Drawing.Point(501, 72)
+        Me.Label1.Location = New System.Drawing.Point(716, 120)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(106, 37)
+        Me.Label1.Size = New System.Drawing.Size(154, 54)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "HRELLe"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(877, 446)
+        Me.Label2.Location = New System.Drawing.Point(1253, 743)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(41, 15)
+        Me.Label2.Size = New System.Drawing.Size(63, 25)
         Me.Label2.TabIndex = 3
         Me.Label2.Text = "Label2"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(877, 399)
+        Me.Label3.Location = New System.Drawing.Point(1253, 665)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(41, 15)
+        Me.Label3.Size = New System.Drawing.Size(63, 25)
         Me.Label3.TabIndex = 4
         Me.Label3.Text = "Label3"
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1184, 761)
+        Me.ClientSize = New System.Drawing.Size(1200, 800)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
--- Space Invaders/Settings.Designer.vb
+++ Space Invaders/Settings.Designer.vb
@@ -30,30 +30,32 @@ Partial Class Settings
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Segoe UI", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label1.Location = New System.Drawing.Point(470, 9)
+        Me.Label1.Location = New System.Drawing.Point(473, 38)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(167, 54)
+        Me.Label1.Size = New System.Drawing.Size(247, 81)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Settings"
         '
         'HScrollBar1
         '
-        Me.HScrollBar1.Location = New System.Drawing.Point(152, 148)
+        Me.HScrollBar1.Location = New System.Drawing.Point(217, 247)
         Me.HScrollBar1.Maximum = 108
         Me.HScrollBar1.Minimum = 1
         Me.HScrollBar1.Name = "HScrollBar1"
-        Me.HScrollBar1.Size = New System.Drawing.Size(900, 25)
+        Me.HScrollBar1.Size = New System.Drawing.Size(1286, 25)
         Me.HScrollBar1.TabIndex = 1
         Me.HScrollBar1.Value = 1
         '
         'Settings
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1184, 761)
+        Me.ClientSize = New System.Drawing.Size(1200, 800)
         Me.Controls.Add(Me.HScrollBar1)
         Me.Controls.Add(Me.Label1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "Settings"
         Me.Text = "Settings"
         Me.ResumeLayout(False)
