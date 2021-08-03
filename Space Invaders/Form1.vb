dd4525719d71c5655315161d51fbf5c0cf1cc5d3
Terence77717 <limterence12@gmail.com>
2021-08-03 15:18:33 +1000

    test

--- Space Invaders/Form1.vb
+++ Space Invaders/Form1.vb
@@ -1,7 +1,5 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.Size = New Size(1200, 800)
-        Settings.Size = New Size(1200, 800)
     End Sub
 
     Private Sub Back_Click(sender As Object, e As EventArgs) Handles Back.Click
