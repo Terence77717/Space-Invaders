4d8485feea27396ff66b6a48bddbbe66e120fe66
Terence77717 <limterence12@gmail.com>
2021-08-09 14:22:43 +1000

    Level selection pushing

--- Space Invaders/levels.Designer.vb
+++ Space Invaders/levels.Designer.vb
@@ -26,16 +26,16 @@ Partial Class levels
         Me.LevelsLabel = New System.Windows.Forms.Label()
         Me.Back = New System.Windows.Forms.Button()
         Me.Levels1 = New System.Windows.Forms.Label()
-        Me.Level2 = New System.Windows.Forms.Label()
-        Me.Level3 = New System.Windows.Forms.Label()
-        Me.Level4 = New System.Windows.Forms.Label()
-        Me.Level5 = New System.Windows.Forms.Label()
-        Me.Level6 = New System.Windows.Forms.Label()
-        Me.Level7 = New System.Windows.Forms.Label()
-        Me.Level8 = New System.Windows.Forms.Label()
+        Me.Levels2 = New System.Windows.Forms.Label()
+        Me.Levels3 = New System.Windows.Forms.Label()
+        Me.Levels4 = New System.Windows.Forms.Label()
+        Me.Levels5 = New System.Windows.Forms.Label()
+        Me.Levels6 = New System.Windows.Forms.Label()
+        Me.Levels7 = New System.Windows.Forms.Label()
+        Me.Levels8 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.Level10 = New System.Windows.Forms.Label()
-        Me.Level9 = New System.Windows.Forms.Label()
+        Me.Levels10 = New System.Windows.Forms.Label()
+        Me.Levels9 = New System.Windows.Forms.Label()
         Me.EndlessMode = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
@@ -45,7 +45,7 @@ Partial Class levels
         Me.LevelsLabel.BackColor = System.Drawing.Color.Transparent
         Me.LevelsLabel.Font = New System.Drawing.Font("Segoe UI", 35.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.LevelsLabel.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.LevelsLabel.Location = New System.Drawing.Point(419, 40)
+        Me.LevelsLabel.Location = New System.Drawing.Point(416, 40)
         Me.LevelsLabel.Name = "LevelsLabel"
         Me.LevelsLabel.Size = New System.Drawing.Size(393, 62)
         Me.LevelsLabel.TabIndex = 18
@@ -78,110 +78,110 @@ Partial Class levels
         Me.Levels1.Text = "Level 01"
         Me.Levels1.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
-        'Level2
+        'Levels2
         '
-        Me.Level2.AutoSize = True
-        Me.Level2.BackColor = System.Drawing.Color.Transparent
-        Me.Level2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Level2.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Level2.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Level2.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Level2.Location = New System.Drawing.Point(458, 258)
-        Me.Level2.Name = "Level2"
-        Me.Level2.Size = New System.Drawing.Size(143, 48)
-        Me.Level2.TabIndex = 21
-        Me.Level2.Text = "Level 02"
-        Me.Level2.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Levels2.AutoSize = True
+        Me.Levels2.BackColor = System.Drawing.Color.Transparent
+        Me.Levels2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Levels2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Levels2.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Levels2.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Levels2.Location = New System.Drawing.Point(458, 258)
+        Me.Levels2.Name = "Levels2"
+        Me.Levels2.Size = New System.Drawing.Size(143, 48)
+        Me.Levels2.TabIndex = 21
+        Me.Levels2.Text = "Level 02"
+        Me.Levels2.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
-        'Level3
+        'Levels3
         '
-        Me.Level3.AutoSize = True
-        Me.Level3.BackColor = System.Drawing.Color.Transparent
-        Me.Level3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Level3.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Level3.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Level3.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Level3.Location = New System.Drawing.Point(458, 325)
-        Me.Level3.Name = "Level3"
-        Me.Level3.Size = New System.Drawing.Size(143, 48)
-        Me.Level3.TabIndex = 22
-        Me.Level3.Text = "Level 03"
-        Me.Level3.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Levels3.AutoSize = True
+        Me.Levels3.BackColor = System.Drawing.Color.Transparent
+        Me.Levels3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Levels3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Levels3.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Levels3.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Levels3.Location = New System.Drawing.Point(458, 325)
+        Me.Levels3.Name = "Levels3"
+        Me.Levels3.Size = New System.Drawing.Size(143, 48)
+        Me.Levels3.TabIndex = 22
+        Me.Levels3.Text = "Level 03"
+        Me.Levels3.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
-        'Level4
+        'Levels4
         '
-        Me.Level4.AutoSize = True
-        Me.Level4.BackColor = System.Drawing.Color.Transparent
-        Me.Level4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Level4.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Level4.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Level4.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Level4.Location = New System.Drawing.Point(458, 390)
-        Me.Level4.Name = "Level4"
-        Me.Level4.Size = New System.Drawing.Size(143, 48)
-        Me.Level4.TabIndex = 23
-        Me.Level4.Text = "Level 04"
-        Me.Level4.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Levels4.AutoSize = True
+        Me.Levels4.BackColor = System.Drawing.Color.Transparent
+        Me.Levels4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Levels4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Levels4.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Levels4.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Levels4.Location = New System.Drawing.Point(458, 390)
+        Me.Levels4.Name = "Levels4"
+        Me.Levels4.Size = New System.Drawing.Size(143, 48)
+        Me.Levels4.TabIndex = 23
+        Me.Levels4.Text = "Level 04"
+        Me.Levels4.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
-        'Level5
+        'Levels5
         '
-        Me.Level5.AutoSize = True
-        Me.Level5.BackColor = System.Drawing.Color.Transparent
-        Me.Level5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Level5.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Level5.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Level5.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Level5.Location = New System.Drawing.Point(458, 456)
-        Me.Level5.Name = "Level5"
-        Me.Level5.Size = New System.Drawing.Size(143, 48)
-        Me.Level5.TabIndex = 24
-        Me.Level5.Text = "Level 05"
-        Me.Level5.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Levels5.AutoSize = True
+        Me.Levels5.BackColor = System.Drawing.Color.Transparent
+        Me.Levels5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Levels5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Levels5.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Levels5.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Levels5.Location = New System.Drawing.Point(458, 456)
+        Me.Levels5.Name = "Levels5"
+        Me.Levels5.Size = New System.Drawing.Size(143, 48)
+        Me.Levels5.TabIndex = 24
+        Me.Levels5.Text = "Level 05"
+        Me.Levels5.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
-        'Level6
+        'Levels6
         '
-        Me.Level6.AutoSize = True
-        Me.Level6.BackColor = System.Drawing.Color.Transparent
-        Me.Level6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Level6.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Level6.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Level6.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Level6.Location = New System.Drawing.Point(623, 193)
-        Me.Level6.Name = "Level6"
-        Me.Level6.Size = New System.Drawing.Size(143, 48)
-        Me.Level6.TabIndex = 25
-        Me.Level6.Text = "Level 06"
-        Me.Level6.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Levels6.AutoSize = True
+        Me.Levels6.BackColor = System.Drawing.Color.Transparent
+        Me.Levels6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Levels6.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Levels6.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Levels6.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Levels6.Location = New System.Drawing.Point(623, 193)
+        Me.Levels6.Name = "Levels6"
+        Me.Levels6.Size = New System.Drawing.Size(143, 48)
+        Me.Levels6.TabIndex = 25
+        Me.Levels6.Text = "Level 06"
+        Me.Levels6.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
-        'Level7
+        'Levels7
         '
-        Me.Level7.AutoSize = True
-        Me.Level7.BackColor = System.Drawing.Color.Transparent
-        Me.Level7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Level7.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Level7.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Level7.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Level7.Location = New System.Drawing.Point(623, 258)
-        Me.Level7.Name = "Level7"
-        Me.Level7.Size = New System.Drawing.Size(143, 48)
-        Me.Level7.TabIndex = 26
-        Me.Level7.Text = "Level 07"
-        Me.Level7.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Levels7.AutoSize = True
+        Me.Levels7.BackColor = System.Drawing.Color.Transparent
+        Me.Levels7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Levels7.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Levels7.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Levels7.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Levels7.Location = New System.Drawing.Point(623, 258)
+        Me.Levels7.Name = "Levels7"
+        Me.Levels7.Size = New System.Drawing.Size(143, 48)
+        Me.Levels7.TabIndex = 26
+        Me.Levels7.Text = "Level 07"
+        Me.Levels7.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
-        'Level8
+        'Levels8
         '
-        Me.Level8.AutoSize = True
-        Me.Level8.BackColor = System.Drawing.Color.Transparent
-        Me.Level8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Level8.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Level8.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Level8.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Level8.Location = New System.Drawing.Point(623, 325)
-        Me.Level8.Name = "Level8"
-        Me.Level8.Size = New System.Drawing.Size(143, 48)
-        Me.Level8.TabIndex = 27
-        Me.Level8.Text = "Level 08"
-        Me.Level8.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Levels8.AutoSize = True
+        Me.Levels8.BackColor = System.Drawing.Color.Transparent
+        Me.Levels8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Levels8.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Levels8.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Levels8.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Levels8.Location = New System.Drawing.Point(623, 325)
+        Me.Levels8.Name = "Levels8"
+        Me.Levels8.Size = New System.Drawing.Size(143, 48)
+        Me.Levels8.TabIndex = 27
+        Me.Levels8.Text = "Level 08"
+        Me.Levels8.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
         'Label4
         '
@@ -198,35 +198,35 @@ Partial Class levels
         Me.Label4.Text = "Level 6"
         Me.Label4.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
-        'Level10
+        'Levels10
         '
-        Me.Level10.AutoSize = True
-        Me.Level10.BackColor = System.Drawing.Color.Transparent
-        Me.Level10.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Level10.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Level10.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Level10.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Level10.Location = New System.Drawing.Point(623, 456)
-        Me.Level10.Name = "Level10"
-        Me.Level10.Size = New System.Drawing.Size(143, 48)
-        Me.Level10.TabIndex = 31
-        Me.Level10.Text = "Level 10"
-        Me.Level10.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Levels10.AutoSize = True
+        Me.Levels10.BackColor = System.Drawing.Color.Transparent
+        Me.Levels10.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Levels10.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Levels10.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Levels10.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Levels10.Location = New System.Drawing.Point(623, 456)
+        Me.Levels10.Name = "Levels10"
+        Me.Levels10.Size = New System.Drawing.Size(143, 48)
+        Me.Levels10.TabIndex = 31
+        Me.Levels10.Text = "Level 10"
+        Me.Levels10.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
-        'Level9
+        'Levels9
         '
-        Me.Level9.AutoSize = True
-        Me.Level9.BackColor = System.Drawing.Color.Transparent
-        Me.Level9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Level9.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Level9.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Level9.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Level9.Location = New System.Drawing.Point(623, 390)
-        Me.Level9.Name = "Level9"
-        Me.Level9.Size = New System.Drawing.Size(143, 48)
-        Me.Level9.TabIndex = 30
-        Me.Level9.Text = "Level 09"
-        Me.Level9.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Levels9.AutoSize = True
+        Me.Levels9.BackColor = System.Drawing.Color.Transparent
+        Me.Levels9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Levels9.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Levels9.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Levels9.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Levels9.Location = New System.Drawing.Point(623, 390)
+        Me.Levels9.Name = "Levels9"
+        Me.Levels9.Size = New System.Drawing.Size(143, 48)
+        Me.Levels9.TabIndex = 30
+        Me.Levels9.Text = "Level 09"
+        Me.Levels9.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
         'EndlessMode
         '
@@ -236,7 +236,7 @@ Partial Class levels
         Me.EndlessMode.Cursor = System.Windows.Forms.Cursors.Hand
         Me.EndlessMode.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.EndlessMode.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.EndlessMode.Location = New System.Drawing.Point(484, 541)
+        Me.EndlessMode.Location = New System.Drawing.Point(482, 541)
         Me.EndlessMode.Name = "EndlessMode"
         Me.EndlessMode.Size = New System.Drawing.Size(264, 48)
         Me.EndlessMode.TabIndex = 32
@@ -250,15 +250,15 @@ Partial Class levels
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.ClientSize = New System.Drawing.Size(1200, 800)
         Me.Controls.Add(Me.EndlessMode)
-        Me.Controls.Add(Me.Level10)
-        Me.Controls.Add(Me.Level9)
-        Me.Controls.Add(Me.Level8)
-        Me.Controls.Add(Me.Level7)
-        Me.Controls.Add(Me.Level6)
-        Me.Controls.Add(Me.Level5)
-        Me.Controls.Add(Me.Level4)
-        Me.Controls.Add(Me.Level3)
-        Me.Controls.Add(Me.Level2)
+        Me.Controls.Add(Me.Levels10)
+        Me.Controls.Add(Me.Levels9)
+        Me.Controls.Add(Me.Levels8)
+        Me.Controls.Add(Me.Levels7)
+        Me.Controls.Add(Me.Levels6)
+        Me.Controls.Add(Me.Levels5)
+        Me.Controls.Add(Me.Levels4)
+        Me.Controls.Add(Me.Levels3)
+        Me.Controls.Add(Me.Levels2)
         Me.Controls.Add(Me.Levels1)
         Me.Controls.Add(Me.Back)
         Me.Controls.Add(Me.LevelsLabel)
@@ -274,15 +274,15 @@ Partial Class levels
     Friend WithEvents LevelsLabel As Label
     Friend WithEvents Back As Button
     Friend WithEvents Levels1 As Label
-    Friend WithEvents Level2 As Label
-    Friend WithEvents Level3 As Label
-    Friend WithEvents Level4 As Label
-    Friend WithEvents Level5 As Label
-    Friend WithEvents Level6 As Label
-    Friend WithEvents Level7 As Label
-    Friend WithEvents Level8 As Label
+    Friend WithEvents Levels2 As Label
+    Friend WithEvents Levels3 As Label
+    Friend WithEvents Levels4 As Label
+    Friend WithEvents Levels5 As Label
+    Friend WithEvents Levels6 As Label
+    Friend WithEvents Levels7 As Label
+    Friend WithEvents Levels8 As Label
     Friend WithEvents Label4 As Label
-    Friend WithEvents Level10 As Label
-    Friend WithEvents Level9 As Label
+    Friend WithEvents Levels10 As Label
+    Friend WithEvents Levels9 As Label
     Friend WithEvents EndlessMode As Label
 End Class
--- Space Invaders/levels.vb
+++ Space Invaders/levels.vb
@@ -11,4 +11,50 @@
             EndlessMode.ForeColor = Color.White
         End If
     End Sub
+    'Level selection
+    Private Sub Levels1_Click(sender As Object, e As EventArgs) Handles Levels1.Click
+        Form1.levelselected = 1
+    End Sub
+
+    Private Sub Levels2_Click(sender As Object, e As EventArgs) Handles Levels2.Click
+        Form1.levelselected = 2
+    End Sub
+
+    Private Sub Levels3_Click(sender As Object, e As EventArgs) Handles Levels3.Click
+        Form1.levelselected = 3
+    End Sub
+
+    Private Sub Levels4_Click(sender As Object, e As EventArgs) Handles Levels4.Click
+        Form1.levelselected = 4
+    End Sub
+
+    Private Sub Levels5_Click(sender As Object, e As EventArgs) Handles Levels5.Click
+        Form1.levelselected = 5
+    End Sub
+
+    Private Sub Levels6_Click(sender As Object, e As EventArgs) Handles Levels6.Click
+        Form1.levelselected = 6
+    End Sub
+
+    Private Sub Levels7_Click(sender As Object, e As EventArgs) Handles Levels7.Click
+        Form1.levelselected = 7
+    End Sub
+
+    Private Sub Levels8_Click(sender As Object, e As EventArgs) Handles Levels8.Click
+        Form1.levelselected = 8
+    End Sub
+
+    Private Sub Levels9_Click(sender As Object, e As EventArgs) Handles Levels9.Click
+        Form1.levelselected = 9
+    End Sub
+
+    Private Sub Levels10_Click(sender As Object, e As EventArgs) Handles Levels10.Click
+        Form1.levelselected = 10
+    End Sub
+
+    Private Sub EndlessMode_Click(sender As Object, e As EventArgs) Handles EndlessMode.Click
+        If Form1.EndlessUnlocked = True Then
+            Form1.levelselected = 0
+        End If
+    End Sub
 End Class
