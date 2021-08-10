3a1ad87a8ea3f428c2f9654e12b544ae5578dadf
Samin Islam <436665504@student.sbhs.nsw.edu.au>
2021-08-10 11:17:56 +1000

    yes

--- Space Invaders/Play.Designer.vb
+++ Space Invaders/Play.Designer.vb
@@ -25,65 +25,51 @@ Partial Class Form2
         Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2))
         Me.player = New System.Windows.Forms.PictureBox()
-        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.movementTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.projectile = New System.Windows.Forms.Label()
         CType(Me.player, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'player
         '
         Me.player.Image = CType(resources.GetObject("player.Image"), System.Drawing.Image)
-        Me.player.Location = New System.Drawing.Point(256, 273)
-        Me.player.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.player.Location = New System.Drawing.Point(573, 703)
         Me.player.Name = "player"
-        Me.player.Size = New System.Drawing.Size(115, 58)
+        Me.player.Size = New System.Drawing.Size(88, 48)
         Me.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.player.TabIndex = 0
         Me.player.TabStop = False
         '
-        'PictureBox1
+        'movementTimer
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(424, 173)
-        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(105, 45)
-        Me.PictureBox1.TabIndex = 1
-        Me.PictureBox1.TabStop = False
         '
-        'Label1
+        'projectile
         '
-        Me.Label1.BackColor = System.Drawing.Color.White
-        Me.Label1.ForeColor = System.Drawing.Color.White
-        Me.Label1.Location = New System.Drawing.Point(361, 253)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(9, 18)
-        Me.Label1.TabIndex = 2
-        Me.Label1.Text = "d"
+        Me.projectile.BackColor = System.Drawing.Color.White
+        Me.projectile.ForeColor = System.Drawing.Color.White
+        Me.projectile.Location = New System.Drawing.Point(613, 680)
+        Me.projectile.Name = "projectile"
+        Me.projectile.Size = New System.Drawing.Size(7, 20)
+        Me.projectile.TabIndex = 2
+        Me.projectile.Visible = False
         '
         'Form2
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Black
-        Me.ClientSize = New System.Drawing.Size(727, 356)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.PictureBox1)
+        Me.ClientSize = New System.Drawing.Size(1255, 763)
+        Me.Controls.Add(Me.projectile)
         Me.Controls.Add(Me.player)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Form2"
         Me.Text = "Game"
         CType(Me.player, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents player As PictureBox
-    Friend WithEvents Timer1 As Timer
-    Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents Label1 As Label
+    Friend WithEvents movementTimer As Timer
+    Friend WithEvents projectile As Label
 End Class
