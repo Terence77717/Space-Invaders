e42b12878721a719f11f327ea06555ebe6bf01b7
Samin Islam <436665504@student.sbhs.nsw.edu.au>
2021-08-30 13:44:47 +1000

    play changes

--- Space Invaders/play.vb
+++ Space Invaders/play.vb
@@ -7,11 +7,13 @@ Public Class Form2
     Dim projOnScreen(numofshots) As Boolean
     Dim playerRight As Boolean = False
     Dim playerLeft As Boolean = False
-
+    Dim maxEnemyNum As Integer = 15
     'heart and game over
     Dim hearts As Integer = 3
     Dim playerhit As Boolean = False
     Dim gameover As Boolean = False
+    Dim enemyArray(maxEnemyNum) As PictureBox
+    Dim enemyOnScreen(maxEnemyNum) As Boolean
 
     Public Function checkhearts() 'add sound effect for getting hit
         If hearts = 3 Then ' if there is 3 heart left
@@ -62,9 +64,10 @@ Public Class Form2
     Public Sub createEnemy(number)
         For i = 0 To number - 1
             Dim enemy As New PictureBox
-            enemy.Size = New Size(20, 20)
-            enemy.BackColor = Color.Red
-            enemy.Top = 50
+            enemy.Size = New Size(40, 40)
+            enemy.Image = My.Resources.alien
+            enemy.SizeMode = PictureBoxSizeMode.StretchImage
+            enemy.Top = 0
             enemy.Left = i * 50
             enemy.BringToFront()
             Me.Controls.Add(enemy)
@@ -109,12 +112,12 @@ Public Class Form2
         Heart1.Location = New Point(1100, 12)
         Heart2.Location = New Point(1137, 12)
         Heart3.Location = New Point(1174, 12)
-        Me.Size = New Size(1277, 819)
+        Me.Size = New Size(1254, 763)
         Me.CenterToScreen()
         tmrShoot.Enabled = True
         tmrmove.Enabled = True
         player.Left = Me.Width / 2 - player.Width / 2
-        player.Top = Me.Height - 2 * player.Height
+        player.Top = Me.Height - player.Height
         player.Size = New Size(88, 48)
         createProj(numofshots)
         createEnemy(maxEnemyNum)
@@ -122,9 +125,9 @@ Public Class Form2
         Heart1.Location = New Point(1100, 20)
         Heart2.Location = New Point(1137, 20)
         Heart3.Location = New Point(1174, 20)
-        Heart1.Size = New Size(40, 20)
-        Heart2.Size = New Size(40, 20)
-        Heart3.Size = New Size(40, 20)
+        Heart1.Size = New Size(40, 40)
+        Heart2.Size = New Size(40, 40)
+        Heart3.Size = New Size(40, 40)
 
 
         createProj(numofshots)
