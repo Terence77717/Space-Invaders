3b28c686e04578f4e43649772b362718e269097a
Terence77717 <limterence12@gmail.com>
2021-09-29 14:55:19 +1000

    added rounds and spawn level module

--- Space Invaders/modulefunc.vb
+++ Space Invaders/modulefunc.vb
@@ -4,11 +4,41 @@
 
     ' internal variables
     Dim number As Integer
+    Dim alienhp As Integer
 
-    Function enemytype(type)
+    Function enemytype(type, amount)
         Select Case type
-            Case 1
-                For i = 0 To number - 1
+            Case 1 'normal alien
+                alienhp = 1
+                For i = 0 To amount - 1
+                    Dim enemy As New PictureBox
+                    enemy.Size = New Size(50, 50)
+                    enemy.BackgroundImageLayout = ImageLayout.Stretch
+                    enemy.BackgroundImage = My.Resources.alien
+                    enemy.Top = 100
+                    enemy.Left = i * 70 + 50
+                    enemy.BringToFront()
+                    'Me.Controls.Add(enemy)
+                    enemywave.Add(enemy)
+                    enemywave(i).Visible = True
+                Next
+            Case 2 'shooting alien
+                alienhp = 1
+                For i = 0 To amount - 1
+                    Dim enemy As New PictureBox
+                    enemy.Size = New Size(50, 50)
+                    enemy.BackgroundImageLayout = ImageLayout.Stretch
+                    enemy.BackgroundImage = My.Resources.alien
+                    enemy.Top = 100
+                    enemy.Left = i * 70 + 50
+                    enemy.BringToFront()
+                    'Me.Controls.Add(enemy)
+                    enemywave.Add(enemy)
+                    enemywave(i).Visible = True
+                Next
+            Case 3 'mothership
+                alienhp = 10
+                For i = 0 To amount - 1
                     Dim enemy As New PictureBox
                     enemy.Size = New Size(50, 50)
                     enemy.BackgroundImageLayout = ImageLayout.Stretch
@@ -26,54 +56,44 @@
         roundused.Clear()
         Select Case level
             Case 1
-                roundused.AddRange({1, 2, 3, 4, 5})
+                roundused.AddRange({1, 1, 7})
             Case 2
-
+                roundused.AddRange({1, 2, 1, 7})
             Case 3
-
+                roundused.AddRange({1, 2, 2, 7})
             Case 4
-
+                roundused.AddRange({1, 2, 4, 1, 7})
             Case 5
-
+                roundused.AddRange({2, 2, 4, 2, 7})
             Case 6
-
+                roundused.AddRange({2, 3, 4, 2, 7})
             Case 7
-
+                roundused.AddRange({2, 3, 4, 3, 7})
             Case 8
-
+                roundused.AddRange({3, 5, 3, 4, 7})
             Case 9
-
+                roundused.AddRange({3, 5, 5, 3, 2, 7})
             Case 10
-
-            Case 11
-
+                roundused.AddRange({3, 6, 3, 6, 3, 7})
         End Select
     End Function
 
     Function spawnround(round)
         Select Case round
             Case 1
-
+                enemytype(1, 10) 'normal aliens
             Case 2
-
+                enemytype(1, 20)
             Case 3
-
+                enemytype(1, 30)
             Case 4
-
+                enemytype(2, 10) 'shooting aliens
             Case 5
-
+                enemytype(2, 20)
             Case 6
-
+                enemytype(2, 30)
             Case 7
-
-            Case 8
-
-            Case 9
-
-            Case 10
-
-            Case 11
-
+                enemytype(3, 1) 'mothership
         End Select
     End Function
 End Module
